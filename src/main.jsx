82fcd9d0a24c395227379967896b6e45fc1b35d2
sherjakhondev <sherjakhon05@gmail.com>
2024-10-23 11:02:00 +0500

    ASD

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,70 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Dashboard from "./Pages/Dashboard";
+import Leads from "./Pages/Leads";
+import Transactions from "./Pages/Transactions";
+import Analytics from "./Pages/Analytics";
+import Integration from "./Pages/Integration";
+import Calendar from "./Pages/Calendar";
+import Pages from "./Pages/Pages";
+import Settings from "./Pages/Settings";
+import Documentation from "./Pages/Documentation";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children:[
+      {
+      path:"/",
+      element:<Dashboard/>,
+    
+    },
+    {
+      path:'/leads',
+      element:<Leads/>,
+    },
+    {
+      path:'/Transactions',
+      element:<Transactions/>,
+    },
+    {
+      path:'/Analytics',
+      element:<Analytics/>,
+    },
+    {
+      path:'/Integration',
+      element:<Integration/>,
+    },
+    {
+      path:'/Calendar',
+      element:<Calendar/>,
+    },
+    {
+      path:'/Pages',
+      element:<Pages/>,
+    },
+    {
+      path:'/Settings',
+      element:<Settings/>,
+    },
+    {
+      path:'/Documentation',
+      element:<Documentation/>,
+    },
+    
+  ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
