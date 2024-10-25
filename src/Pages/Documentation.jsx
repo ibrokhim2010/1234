9b69c197b73acd6085e40e9a35eaed5eca89d565
sherjakhondev <sherjakhon05@gmail.com>
2024-10-25 09:59:04 +0500

    AS

--- src/Pages/Documentation.jsx
+++ src/Pages/Documentation.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { BsApple } from "react-icons/bs";
+
 
 const Documentation = () => {
     return (
         <div className='flex  items-center justify-center h-full min-h-[85vh] text-9xl text-blue-400'>
-            <Link to="/Facts">
-            <BsApple   className='text-blue-900 hover:shadow-lg hover:shadow-cyan-400 rounded-full hover:border-2 hover:border-cyan-400 active:scale-75 active:transition-all' />
-            
+           <Link to="/Facts">
+           
+            <button  className=" px-10 py-9 font-300 text-md bg-blue-500 text-white rounded-lg border-blue-900 border-4">Bosgan Gey</button>
             </Link>
+            
+            
+           
             
           
         </div>
